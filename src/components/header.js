6777d50785577f5d68e74e33bb8685cd96f8e676
Ellis Collinson <EllisCollinson@me.com>
2022-02-15 15:04:36 +0000

    init: initial push

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+// importing images
+import Logo from "../images/cd_logo_white.png";
+import Icon from "../images/cd_icon_white.png";
+
+export default function Header() {
+  return (
+    <div className="App-header">
+      <div className="App-header-inner">
+        <div className="Item1 full-center">
+          <img src={Icon} className="Icon-main"></img>
+        </div>
+        <div className="Item2 full-center">Menu</div>
+      </div>
+      <img src={Logo} alt="Logo" className="Logo-main" />
+	  The UK's best events, brewed to perfection
+    </div>
+  );
+}
---
+++ src/views/home.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Header from "../components/header";
+
+// importing components
+import header from "../components/header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
